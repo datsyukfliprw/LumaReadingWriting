e22e8523357e2df15f621544046a4e0f05a5a8ac
datsyukfliprw <122399464+datsyukfliprw@users.noreply.github.com>
2026-09-03 13:54:34 -0500

    Isolate solver worker types

--- src/workers/solver.worker.ts
+++ src/workers/solver.worker.ts
@@ -1,10 +1,15 @@
-/// <reference lib="webworker" />
-
 import { dictionary } from '../game/dictionary'
 import { solveBoard } from '../game/solver'
 import type { Board } from '../game/types'
 
-self.onmessage = (event: MessageEvent<Board>) => {
+type SolverWorkerScope = {
+  onmessage: ((event: MessageEvent<Board>) => void) | null
+  postMessage: (message: string[]) => void
+}
+
+const workerScope = self as unknown as SolverWorkerScope
+
+workerScope.onmessage = (event) => {
   const solutions = solveBoard(event.data, dictionary, 5000)
-  self.postMessage(solutions)
+  workerScope.postMessage(solutions)
 }
